bb8696c010b539dd6a8de42ce6c9b464ff3dd2cb
huang <huangice0519@gmail.com>
2022-11-17 09:05:32 +0800

    change SQLite to postgres

--- main.py
+++ main.py
@@ -14,8 +14,8 @@ import os
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'HUANGQUAN880519'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///cafes.db'
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 bootstrap = Bootstrap(app)
